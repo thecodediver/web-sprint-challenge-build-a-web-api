765a10f95e8431bad2295cae8c97d5d358f35a0b
James <info@thecodediver.com>
2020-12-03 22:28:03 -0500

    updates information required to implement endpoints

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,48 @@
-// Write your "actions" router here!
+const router = require("express").Router()
+const Actions = require("./actions-model")
+
+router.get("/", (req, res) => {
+  Actions.get()
+  .then(data => {
+    res.status(200).json(data)
+  })
+  .catch(err => {
+    res.status(500).json({ err: err.message})
+  })
+})
+
+router.get("/:id", (req, res) => {
+  Actions.get(req.params.id)
+  .then(data => {
+    res.status(200).json(data)
+  })
+  .catch(err => {
+    res.status(500).json(err.message)
+  })
+})
+
+router.post("/", (req, res) => {
+  Actions.insert(req.body)
+  .then(data => {
+    res.status(200).json(data)
+  })
+  .catch(err => {
+    res.status(500).json(err.message)
+  })
+})
+
+router.put("/", (req, res) => {
+  Actions.update(req.id, req.body)
+  .then(data => {
+    res.status(200).json(data)
+  })
+})
+
+router.delete("/", (req, res) => {
+  Actions.remove(req.body.id)
+  .then(() => {
+    res.status(200)
+  })
+})
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,58 @@
-// Write your "projects" router here!
+const router = require("express").Router()
+const Projects = require("./projects-model")
+
+router.get("/", (req, res) => {
+  Projects.get()
+  .then(data => {
+    res.status(200).json(data)
+  })
+  .catch(err => {
+    res.status(500).json(err.message)
+  }) 
+})
+
+router.get("/:id", (req, res) => {
+  Projects.get(req.params.id)
+  .then(data => {
+    res.status(200).json(data)
+  })
+  .catch(err => {
+    res.status(500).json(err.message)
+  })
+})
+
+router.post("/", (req, res) => {
+  Projects.insert(req.body)
+  .then(data => {
+    res.status(201).json(data)
+  })
+  .catch(err => {
+    res.status(500).json(err.message)
+  })
+})
+
+router.put("/", (req, res) => {
+  Projects.update(req.body)
+  .then(data => {
+    res.status(200).json(data)
+  })
+  .catch(err => {
+    res.status(500).json(err.message)
+  })
+})
+
+router.delete("/", (req, res) => {
+  Projects.remove(req.id)
+  .then(() => {
+    res.status(200)
+  })
+  .catch(err => {
+    res.status(500).json(err.message)
+  })
+})
+
+router.get("/:id/actions", (req, res) => {
+  res.status(200).json({ err: " You did it"})
+})
+
+module.exports = router
